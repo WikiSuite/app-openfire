90548a069ab5a30a3ba6086229d6dfa83ce782fb
pcbaldwin <peter@egloo.ca>
2016-12-23 14:06:45 -0500

    Add base API to make buildsys happy

---
+++ libraries/Openfire.php
@@ -0,0 +1,90 @@
+<?php
+
+/**
+ * Openfire server class.
+ *
+ * @category   apps
+ * @package    openfire
+ * @subpackage controllers
+ * @author     Marc Laporte
+ * @copyright  2016 Marc Laporte
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       https://github.com/eglooca/app-openfire
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+///////////////////////////////////////////////////////////////////////////////
+
+///////////////////////////////////////////////////////////////////////////////
+// N A M E S P A C E
+///////////////////////////////////////////////////////////////////////////////
+
+namespace clearos\apps\openfire;
+
+///////////////////////////////////////////////////////////////////////////////
+// B O O T S T R A P
+///////////////////////////////////////////////////////////////////////////////
+
+$bootstrap = getenv('CLEAROS_BOOTSTRAP') ? getenv('CLEAROS_BOOTSTRAP') : '/usr/clearos/framework/shared';
+require_once $bootstrap . '/bootstrap.php';
+
+///////////////////////////////////////////////////////////////////////////////
+// T R A N S L A T I O N S
+///////////////////////////////////////////////////////////////////////////////
+
+clearos_load_language('openfire');
+
+///////////////////////////////////////////////////////////////////////////////
+// D E P E N D E N C I E S
+///////////////////////////////////////////////////////////////////////////////
+
+// Classes
+//--------
+
+use \clearos\apps\base\Daemon as Daemon;
+
+clearos_load_library('base/Daemon');
+
+///////////////////////////////////////////////////////////////////////////////
+// C L A S S
+///////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Openfire server class.
+ *
+ * @category   apps
+ * @package    openfire
+ * @subpackage controllers
+ * @author     Marc Laporte
+ * @copyright  2016 Marc Laporte
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU General Public License version 3 or later
+ * @link       https://github.com/eglooca/app-openfire
+ */
+
+class Openfire extends Daemon
+{
+    /**
+     * Openfire constructor.
+     */
+
+    public function __construct()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        parent::__construct('openfire');
+    }
+}
