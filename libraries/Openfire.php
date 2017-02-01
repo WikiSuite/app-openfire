2a015be89b347bbcbdc96ba5567350a584e8d091
pcbaldwin <peter@egloo.ca>
2017-01-31 22:17:01 -0500

    Moved set properties to API

--- libraries/Openfire.php
+++ libraries/Openfire.php
@@ -56,15 +56,21 @@ clearos_load_language('openfire');
 //--------
 
 use \clearos\apps\base\Daemon as Daemon;
+use \clearos\apps\base\File as File;
 use \clearos\apps\groups\Group_Engine as Group_Engine;
 use \clearos\apps\groups\Group_Manager_Factory as Group_Manager_Factory;
-use \clearos\apps\network\Network_Utils as Network_Utils;
+use \clearos\apps\ldap\LDAP_Factory as LDAP_Factory;
+use \clearos\apps\network\Domain as Domain;
+use \clearos\apps\network\Hostname as Hostname;
 use \clearos\apps\system_database\System_Database as System_Database;
 
 clearos_load_library('base/Daemon');
+clearos_load_library('base/File');
 clearos_load_library('groups/Group_Engine');
 clearos_load_library('groups/Group_Manager_Factory');
-clearos_load_library('network/Network_Utils');
+clearos_load_library('ldap/LDAP_Factory');
+clearos_load_library('network/Domain');
+clearos_load_library('network/Hostname');
 clearos_load_library('system_database/System_Database');
 
 // Exceptions
@@ -97,6 +103,7 @@ class Openfire extends Daemon
     ///////////////////////////////////////////////////////////////////////////////
 
     const DATABASE = 'openfire';
+    const FILE_CONFIG = '/usr/share/openfire/conf/openfire.xml';
     const PROPERTY_ADMINS = 'admin.authorizedJIDs';
 
     ///////////////////////////////////////////////////////////////////////////////
@@ -146,11 +153,14 @@ class Openfire extends Daemon
         $manager = Group_Manager_Factory::create();
         $groups = $manager->get_details(Group_Engine::FILTER_ALL);
 
+        $ldap = LDAP_Factory::create();
+        $base_domain = $ldap->get_base_internet_domain(); // TODO: what is this base_domain AD mode?
+
         $list = [];
 
         if (array_key_exists('openfire_plugin', $groups)) {
             foreach ($groups['openfire_plugin']['core']['members'] as $username) {
-                $openfire_username = $username . '@' . 'clear7.lan'; // FIXME: what is this in AD mode?
+                $openfire_username = $username . '@' . $base_domain;
                 $list[$openfire_username] = $username;
             }
         }
@@ -172,6 +182,67 @@ class Openfire extends Daemon
         Validation_Exception::is_valid($this->validate_username($username));
 
         $this->_set_property(self::PROPERTY_ADMINS, $username);
+    }
+
+    /**
+     * Updates properties when underlying configuration changes.
+     *
+     * If an adminstrator makes changes to the ClearOS system, some
+     * properties need to be updated.  For example, if the underlying LDAP
+     * base DN changes, the ldap.adminDN property needs to be updated.
+     *
+     * @return void
+     * @throws Engine_Exception
+     */
+
+    public function update_properties()
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        // Update domain and hostname information
+        //---------------------------------------
+
+        $domain = new Domain();
+        $xmpp_domain = $domain->get_default();
+
+        $hostname = new Hostname();
+        $xmpp_fqdn = $hostname->get_internet_hostname();
+
+        $this->_set_property('xmpp.domain', $xmpp_domain);
+        $this->_set_property('xmpp.fqdn', $xmpp_fqdn);
+
+        // Update LDAP base DN and search filter
+        //--------------------------------------
+        // TODO: add Active Directory support for search_filter
+
+        $ldap = LDAP_Factory::create();
+        $base_dn = $ldap->get_base_dn();
+        $search_filter = '(memberof=cn=openfire_plugin,ou=Groups,ou=Accounts,' . $base_dn . ')';
+
+        $this->_set_property('ldap.baseDN', $base_dn);
+        $this->_set_property('ldap.searchFilter', $search_filter);
+
+        // Update domain used in username
+        //-------------------------------
+        // TODO: Active Directory probably does something different here
+
+        $base_domain = $ldap->get_base_internet_domain();
+        $admin = $this->get_admin();
+        $new_admin = preg_replace('/@.*/', '@' . $base_domain, $admin);
+
+        $this->set_admin($new_admin);
+
+        // Update adminDN and adminPassword via openfire.xml
+        //--------------------------------------------------
+        // TODO: more Active Directory changes required
+
+        $this->_set_xml('adminDN', 'cn=manager,ou=Internal,' . $base_dn);
+        $this->_set_xml('adminPassword', 'jFZ3/Hce3LA7lE77');
+
+        // Restart Openfire
+        //-----------------
+
+        $this->reset(FALSE);
     }
 
     ///////////////////////////////////////////////////////////////////////////////
@@ -245,5 +316,62 @@ class Openfire extends Daemon
 
         $query = "UPDATE ofProperty SET propValue='$value' WHERE name='$property';";
         $system_database->run_update(self::DATABASE, $query);
+    }
+
+    /**
+     * Sets special LDAP values in openfire.xml.
+     *
+     * @param string $property property
+     * @param string $value value
+     *
+     * @return void
+     * @throws Engine_Exception
+     */
+
+    protected function _set_xml($property, $value)
+    {
+        clearos_profile(__METHOD__, __LINE__);
+
+        // TODO: pull in webconfig-php-xml parser in a future release.
+        // For now, just hack it in so we can avoid a webconfig restart.
+
+        $file = new File(self::FILE_CONFIG);
+        $contents = $file->get_contents_as_array();
+        $ldap_found = FALSE;
+        $in_ldap = FALSE;
+        $new = [];
+
+        foreach ($contents as $line) {
+            if ($in_ldap && (preg_match('/<' . $property . '>/', $line)))
+                continue;
+
+            $new[] = $line;
+
+            if (preg_match('/<ldap>/', $line)) {
+                $new[] = '    <' . $property . '>' . $value . '</' . $property . '>';
+                $ldap_found = TRUE;
+                $in_ldap = TRUE;
+            }
+        }
+
+        if (!$ldap_found) {
+            $new = [];
+
+            foreach ($contents as $line) {
+                if (preg_match('/<\/jive>/', $line)) {
+                    $new[] = '  <ldap>';
+                    $new[] = '    <' . $property . '>' . $value . '</' . $property . '>';
+                    $new[] = '  </ldap>';
+                }
+
+                $new[] = $line;
+            }
+        }
+
+        if ($file->exists())
+            $file->delete();
+
+        $file->create('openfire', 'openfire', '0644');
+        $file->add_lines(implode("\n", $new));
     }
 }
