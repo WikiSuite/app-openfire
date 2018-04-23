b24f056a51727fc3753ceb2cd3fb7297b1fe6985
Benjamin Chambers <benjamin@egloo.ca>
2018-04-23 15:40:44 -0400

    Fixed logo

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'openfire';
-$app['version'] = '1.2.8';
+$app['version'] = '1.2.9';
 $app['release'] = '1';
 $app['vendor'] = 'WikiSuite';
 $app['packager'] = 'eGloo';
