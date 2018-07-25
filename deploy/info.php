26587701660e07b7e27f786b7727594191204195
pcbaldwin <peter@egloo.ca>
2018-07-24 22:06:12 -0400

    Buildsys retry

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'openfire';
-$app['version'] = '1.2.15';
+$app['version'] = '1.2.16';
 $app['release'] = '1';
 $app['vendor'] = 'WikiSuite';
 $app['packager'] = 'eGloo';
--- packaging/app-openfire.spec
+++ packaging/app-openfire.spec
@@ -1,7 +1,7 @@
 
 Name: app-openfire
 Epoch: 1
-Version: 1.2.15
+Version: 1.2.16
 Release: 1%{dist}
 Summary: Openfire
 License: GPLv3
