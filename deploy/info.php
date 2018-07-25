bbc1cc1118a394e6d880b6f6b4c11cc834b9e411
pcbaldwin <peter@egloo.ca>
2018-07-24 21:47:23 -0400

    Buildsys retry

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'openfire';
-$app['version'] = '1.2.13';
+$app['version'] = '1.2.14';
 $app['release'] = '1';
 $app['vendor'] = 'WikiSuite';
 $app['packager'] = 'eGloo';
